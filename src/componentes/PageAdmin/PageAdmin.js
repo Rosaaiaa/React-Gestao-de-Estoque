51f0c81520569984aa87c4ae1b1e78f0b08c072b
RenoirBagi <renoirinvest@gmail.com>
2025-11-09 18:13:11 -0300

    Adequação do estilo do Relatório em PageAdmin

--- src/componentes/PageAdmin/PageAdmin.js
+++ src/componentes/PageAdmin/PageAdmin.js
@@ -1,13 +1,30 @@
+import "./PageAdmin.css"
 import LateralBar from "../LateralBar/LateralBar";
 import ReportSaleByProduct from "../ReportSaleByProduct/ReportSaleByProduct";
 
 function PageAdmin() {
 
     return(
-        <>
-        <LateralBar></LateralBar>
-        <ReportSaleByProduct></ReportSaleByProduct>
-        </>
+        <div className="dashboard-layout">
+
+            <LateralBar></LateralBar>
+      
+            <div className="conteudo">
+                <h1>Dashboard</h1>
+
+                <div className="reports-container">
+                <div className="report-card">
+                    <h2>Relatório de Vendas por Produto</h2>
+                    <ReportSaleByProduct /> 
+                </div>
+
+                <div className="report-card">
+                    <h2>Relatório 2 (Em breve)</h2>
+                    <p>Conteúdo do segundo relatório.</p>
+                </div>
+                </div>
+            </div>
+        </div>
     );
 
 };
